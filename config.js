162b99b5d64220ce44dd32bdf35500ea65e548dd
Virgim17 <virgim17@gmail.com>
2026-07-07 23:38:40 -0400

    Update RADIO_CONFIG with new social media links
    Added additional social media links to the configuration.

--- config.js
+++ config.js
@@ -1,15 +1,46 @@
+// =========================================================
+// CONFIGURACIÓN CENTRALIZADA DE RADIO CREPUSCULAR
+// Modifica estos valores para cambiar el sitio sin tocar el código
+// =========================================================
+
 window.RADIO_CONFIG = {
-  streamUrl: 'http://88.150.230.110:27223/stream',
+  // Información de la radio
   nombre: 'Radio Crepuscular',
   frecuencia: '99.5 FM',
   slogan: '☭ ¡Somos Las voces, Voces Combativas! ✊🚩',
+  descripcion: 'La radio del pueblo, en el aire y en la lucha.',
+  
+  // Stream de audio (Shoutcast)
+  streamUrl: 'http://88.150.230.110:27223/stream',
+  
+  // Logo e imágenes
+  logo: '/assets/imagenes/logo.png',
+  
+  // Contacto
+  contacto: {
+    direccion: 'Caracas, Venezuela',
+    telefono: '+58 000 000 0000',
+    email: 'contacto@radiocrepuscular.fm',
+    whatsapp: '+580000000000'
+  },
+  
+  // Redes sociales
   redes: {
     facebook: 'https://facebook.com/radiocrepuscular',
     instagram: 'https://instagram.com/radiocrepuscular',
-    twitter: 'https://twitter.com/radiocrepuscular'
+    twitter: 'https://twitter.com/radiocrepuscular',
+    youtube: 'https://youtube.com/@radiocrepuscular',
+    tiktok: 'https://tiktok.com/@radiocrepuscular'
   },
-  contacto: {
-    email: 'contacto@radiocrepuscular.fm',
-    telefono: '+58 000 0000000'
+  
+  // Colores (puedes cambiarlos aquí)
+  colores: {
+    naranja: '#FF6B35',
+    naranjaClaro: '#FF8C42',
+    sol: '#FFD93D',
+    purpura: '#6B2D5C',
+    purpuraProfundo: '#3D1A46',
+    azulNoche: '#1A1A2E',
+    rojo: '#C1121F'
   }
 };
