e6a4dfbddf0b113e27797af8513fd52df104c544
Virgim17 <virgim17@gmail.com>
2026-07-07 23:18:23 -0400

    Add RADIO_CONFIG for Radio Crepuscular

---
+++ config.js
@@ -0,0 +1,15 @@
+window.RADIO_CONFIG = {
+  streamUrl: 'http://88.150.230.110:27223/stream',
+  nombre: 'Radio Crepuscular',
+  frecuencia: '99.5 FM',
+  slogan: '☭ ¡Somos Las voces, Voces Combativas! ✊🚩',
+  redes: {
+    facebook: 'https://facebook.com/radiocrepuscular',
+    instagram: 'https://instagram.com/radiocrepuscular',
+    twitter: 'https://twitter.com/radiocrepuscular'
+  },
+  contacto: {
+    email: 'contacto@radiocrepuscular.fm',
+    telefono: '+58 000 0000000'
+  }
+};
